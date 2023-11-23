fd3e8f1a2779fcfb3f21b22fda1f2b74ea6294aa
Will-Tiller <104451268+Will-Tiller@users.noreply.github.com>
2023-11-23 22:07:49 +0200

    agora

--- Will/Backend/Filemanager.php
+++ Will/Backend/Filemanager.php
@@ -101,7 +101,7 @@ class Filemanager
             echo "Nenhum arquivo encontrado.";
         }
 
-        
+
     }
 
     public function getArquivos()
@@ -142,7 +142,8 @@ class Filemanager
     }
 
 
-    public function getbyId() {
+    public function getbyId()
+    {
 
         $totalArquivos = $this->conexao->query("SELECT * FROM ficheiros") or die($this->conexao->error);
 
@@ -179,9 +180,10 @@ class Filemanager
 
     }
 
-    public function getnotreguralized() {
+    public function getnotreguralized()
+    {
 
-        
+
         $query = "SELECT COUNT(*) as count FROM ficheiros WHERE estado = 'Em Processamento'";
         $result = $this->conexao->query($query);
 
@@ -192,13 +194,14 @@ class Filemanager
 
         } else {
 
-            return 0; 
+            return 0;
         }
     }
 
-    public function getreguralized() {
+    public function getreguralized()
+    {
 
-        
+
         $query = "SELECT COUNT(*) as count FROM ficheiros WHERE estado = 'Processado'";
         $result = $this->conexao->query($query);
 
@@ -209,13 +212,14 @@ class Filemanager
 
         } else {
 
-            return 0; 
+            return 0;
         }
     }
 
-    public function gettotal() {
+    public function gettotal()
+    {
 
-        
+
         $query = "SELECT COUNT(*) as count FROM ficheiros";
         $result = $this->conexao->query($query);
 
@@ -226,12 +230,69 @@ class Filemanager
 
         } else {
 
-            return 0; 
+            return 0;
         }
     }
 
+    public function getpercategoria()
+    {
+
+        $totalArquivos = $this->conexao->query("SELECT * FROM ficheiros ORDER BY data_submissao DESC LIMIT 5") or die($this->conexao->error);
+
+
+        if ($totalArquivos->num_rows > 0) {
+            echo '<table class="table">';
+            echo '<thead>
+                    <tr>
+                        <th scope="col">Categoria</th>
+                        <th scope="col">Arquivo</th>
+                        <th scope="col">Tramitador</th>
+                        <th scope="col">Estado</th>
+                        <th scope="col">Data de Submissão</th>
+                        <th scope="col">Destinatário</th>
+                        <th scope="col">Ações</th>
+                    </tr>
+                </thead>';
+
+            while ($file = $totalArquivos->fetch_assoc()) {
+                echo "<tr>";
+                echo "<td>{$file['categoria']}</td>";
+                echo "<td>{$file['arquivo']}</td>";
+                echo "<td>{$file['tramitador']}</td>";
+                echo "<td>{$file['estado']}</td>";
+                echo "<td>{$file['data_submissao']}</td>";
+                echo "<td>{$file['destinatario']}</td>";
+                echo "<td><a href='Backend/detalhes_arquivo.php?id={$file['id']}'><button>Selecionar</button></a></td>";
+                echo "</tr>";
+            }
+
+            echo '</table>';
+        } else {
+            echo "Nenhum arquivo encontrado.";
+        }
+
 
     }
+
+    public function contmemorandos()
+    {
+
+        $query = "SELECT COUNT(*) as count FROM ficheiros WHERE estado = 'Processado'";
+        $result = $this->conexao->query($query);
+
+        if ($result) {
+            $row = $result->fetch_assoc();
+
+            return $row['count'];
+
+        } else {
+
+            return 0;
+        }
+
+    }
+
+}
 
 
 ?>
--- Will/Dashboard.php
+++ Will/Dashboard.php
@@ -8,12 +8,18 @@ $file = new Filemanager($conexao);
 
 <!DOCTYPE html>
 <html>
+
 <head>
   <meta charset="UTF-8">
   <title>Sistema Secretaria FET</title>
-  
+
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <link rel="stylesheet" href="assets/style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+    integrity="sha384-ez0wmMyiM8ip9Q4zpoxH4Tsd83e7UFegjRr8RbF9w3jvCWsDxmBAKc83d4tQQZWO" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+    integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body>
@@ -22,68 +28,72 @@ $file = new Filemanager($conexao);
     <nav class="navbar navbar-dark bg-dark">
       <div class="container">
         <a class="navbar-brand" href="#">
-          <img src="logo.png" width="30" height="30" class="d-inline-block align-top">  
-          <span>Sistema da Secretaria da FET</span>   
+          <img src="logo.png" width="30" height="30" class="d-inline-block align-top">
+          <span>Sistema da Secretaria da FET</span>
         </a>
       </div>
     </nav>
   </header>
-  
+
   <div class="sidebar">
-        <h3><a href="index.php">FET</a></h3>
-        <ul>
-            <li>
-                <a href="dashboard.php">Dashboard</a>
-            </li>
-            <li>
-                <li>
-                    <a href="">Tramitação </a>
-                </li>
-                <ul>
-                    <li><a href="acompanhar.php">Acompanhar Progresso</a></li>
-                    <li><a href="registrar.php">Registrar Arquivo</a></li>
-                </ul>
-            </li>
-            <li>
-                <a href="arquivos.php">Arquivos</a>
-            </li>
-            <li>
-                <a href="estatistica.php">Estatísticas </a>
-            </li>
-            <li>
-                <a href="perfil.php">
-                    <img src="user.png" width="20" />
-                    Meu Perfil
-                </a>
-            </li>
-            <li>
-                <a href="logout.php">
-                    <img src="logout.png" width="20" />
-                    Sair
-                </a>
-            </li>
-        </ul>
-    </div>
+    <h3><a href="index.php">FET</a></h3>
+    <ul>
+      <li>
+        <a href="dashboard.php">Dashboard</a>
+      </li>
+      <li>
+      <li>
+        <a href="">Tramitação </a>
+      </li>
+      <ul>
+        <li><a href="acompanhar.php">Acompanhar Progresso</a></li>
+        <li><a href="registrar.php">Registrar Arquivo</a></li>
+      </ul>
+      </li>
+      <li>
+        <a href="arquivos.php">Arquivos</a>
+      </li>
+      <li>
+        <a href="estatistica.php">Estatísticas </a>
+      </li>
+      <li>
+        <a href="perfil.php">
+          <img src="user.png" width="20" />
+          Meu Perfil
+        </a>
+      </li>
+      <li>
+        <a href="logout.php">
+          <img src="logout.png" width="20" />
+          Sair
+        </a>
+      </li>
+    </ul>
+  </div>
 
   <div class="content">
 
     <h1>Dashboard</h1>
-    
+
     <div class="row">
-  
+
       <div class="col-md-3">
         <div class="card bg-primary text-white">
           <div class="card-body">
-          <h2><?php echo $file->gettotal(); ?></h2>
+            <h2>
+              <?php echo $file->gettotal(); ?>
+            </h2>
             <p>Total de Arquivos</p>
           </div>
         </div>
       </div>
-  
+
       <div class="col-md-3">
         <div class="card bg-success  text-white">
           <div class="card-body">
-          <h2><?php echo $file->getreguralized(); ?></h2> 
+            <h2>
+              <?php echo $file->getreguralized(); ?>
+            </h2>
             <p>Arquivos Regularizados</p>
           </div>
         </div>
@@ -92,45 +102,208 @@ $file = new Filemanager($conexao);
       <div class="col-md-3">
         <div class="card bg-warning  text-white">
           <div class="card-body">
-            <h2><?php echo $file->getnotreguralized(); ?></h2> 
+            <h2>
+              <?php echo $file->getnotreguralized(); ?>
+            </h2>
             <p>Arquivos Pendentes</p>
           </div>
         </div>
       </div>
-  
+
     </div><br>
-  
+
     <div class="row">
       <div class="col-md-8">
+        <div class="card">
+          <div class="card-header">
+            Últimos Documentos
+          </div>
+          <div class="card-body table-responsive">
+
+
+            <?php
+
+
+            $file = new Filemanager($conexao); // Passar a conexão do banco de dados
+            $file->getpercategoria();
+
+            ?>
+
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-4">
         <div class="card">
           <div class="card-header">
             Documentos por Tipo
           </div>
           <div class="card-body">
-            <canvas id="myChart"></canvas>
+            <div class="doc-types">
+
+              <div class="row">
+
+                <div class="col">
+
+                  <h4><a href="">Memorandos:</a> 4</h4>
+
+                </div>
+
+                <div class="col">
+
+                  <h4><a href="">Convintes:</a> 4</h4>
+
+                </div>
+
+              </div>
+
+            </div>
+            <div class="doc-types">
+
+              <div class="row">
+
+                <div class="col">
+
+                  <h4><a href="">Pedidos:</a> 4</h4>
+
+                </div>
+
+                <div class="col">
+
+                  <h4><a href="">Informes:</a> 4</h4>
+
+                </div>
+
+              </div>
+
+            </div>
+            <div class="doc-types">
+
+              <div class="row">
+
+                <div class="col">
+
+                  <h4><a href="">Processos Acadêmicos:</a> 4</h4>
+
+                </div>
+
+                <div class="col">
+
+                  <h4><a href="">Contratos e convênios:</a> 4</h4>
+
+                </div>
+
+              </div>
+
+            </div>
+            <div class="doc-types">
+
+              <div class="row">
+
+                <div class="col">
+
+                  <h4><a href="">Editais:</a> 4</h4>
+
+                </div>
+
+                <div class="col">
+
+                  <h4><a href="">Actas:</a> 4</h4>
+
+                </div>
+
+              </div>
+
+            </div>
+            <div class="doc-types">
+
+              <div class="row">
+
+                <div class="col">
+
+                  <h4><a href="">Relatórios:</a> 4</h4>
+
+                </div>
+
+                <div class="col">
+
+                  <h4><a href="">Requerimentos e declarações:</a> 4</h4>
+
+                </div>
+
+              </div>
+
+            </div>
+
+            <div class="doc-types">
+
+              <div class="row">
+
+                <div class="col">
+
+                  <h4><a href="">Relatórios:</a> 4</h4>
+
+                </div>
+
+                <div class="col">
+
+                  <h4><a href="">Requerimentos e declarações:</a> 4</h4>
+
+                </div>
+
+              </div>
+
+            </div>
+
+            <div class="doc-types">
+
+              <div class="row">
+
+                <div class="col">
+
+                  <h4><a href="">Documentos de RH:</a> 4</h4>
+
+                </div>
+
+                <div class="col">
+
+                  <h4><a href="">Notas fiscais e facturas:</a> 4</h4>
+
+                </div>
+
+              </div>
+
+            </div>
+
+            <div class="doc-types">
+
+              <div class="row">
+
+                <div class="col">
+
+                  <h4><a href="">Correspondências diversa:</a> 4</h4>
+
+                </div>
+
+                <div class="col">
+
+                  <h4><a href="">Todos:</a> 4</h4>
+
+                </div>
+
+              </div>
+
+            </div>
           </div>
         </div>
       </div>
-  
-      <div class="col-md-4">
-        <div class="card">
-          <div class="card-header">
-            Últimos Documentos
-          </div>
-          <div class="card-body">
-            <ul class="list-group list-group-flush">
-              <li class="list-group-item">Doc 1234 - Lorem</li>
-              <li class="list-group-item">Doc 4567 - Ipsum</li>
-            </ul>
-          </div>  
-        </div>    
-      </div>
-  
+
     </div>
-    
+
   </div>
 
   <script src="assets/app.js"></script>
 
 </body>
+
 </html>
