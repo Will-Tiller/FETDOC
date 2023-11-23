cc844a43e7baef3c4d373111571718fd4aa26808
arquimedes-admin <arquimedes.69.rufino@gmail.com>
2023-11-23 14:13:24 +0200

    Hi!

---
+++ new/script.js
@@ -0,0 +1,7 @@
+function registerDocument() {
+    // Lógica para registrar o documento e atualizar o dashboard
+    alert('Documento registrado com sucesso!');
+    // Você pode adicionar lógica adicional para atualizar o dashboard com os dados registrados.
+}
+
+// Adicione mais funções JavaScript conforme necessário
