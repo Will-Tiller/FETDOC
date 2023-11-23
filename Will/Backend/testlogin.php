581f863a18a094493f51d85d93b72dea66b40450
Will-Tiller <104451268+Will-Tiller@users.noreply.github.com>
2023-11-24 01:30:36 +0200

    agora 1:30

--- Will/Backend/testlogin.php
+++ Will/Backend/testlogin.php
@@ -1,36 +1,39 @@
 <?php
-
 session_start();
-include_once('conexao.php');
+include_once('Backend/conexao.php');
 
-class LoginController {
+class LoginController
+{
     private $conexao;
 
-    public function __construct($conexao) {
+    public function __construct($conexao)
+    {
         $this->conexao = $conexao;
     }
 
-    public function autenticar($usuario, $senha) {
-
+    public function autenticar($usuario, $senha)
+    {
         $sql = "SELECT * FROM users WHERE nome = '$usuario' AND senha = '$senha'";
-
         $result = $this->conexao->query($sql);
 
         if ($result->num_rows > 0) {
+            // Obter os dados do usuário
+            $dadosUsuario = $result->fetch_assoc();
 
+            // Armazenar os dados do usuário na sessão
+            $_SESSION['usuario'] = $dadosUsuario;
+
+            // Redirecionar para a página do painel
             header("Location: dashboard.php");
             exit();
         } else {
-
             $erro = "Credenciais incorretas. Tente novamente.";
             return $erro;
         }
     }
 }
 
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
     include_once('Backend/conexao.php');
     $loginController = new LoginController($conexao);
 
